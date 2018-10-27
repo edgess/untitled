9331abfbc36ae700d289c71e11798a7c84843e38
edge <edge2@126.com>
2018-10-27 21:56:39 +0800

    123

--- dic4.py
+++ dic4.py
@@ -1,20 +1,17 @@
 import os
 
-pds=[]
+pds = []
 
-
-rg=range(0,10)
-
+rg = range(0, 10)
 
 for three in rg:
     for four in rg:
         for five in rg:
             for six in rg:
-                num= "%s%s%s%s"%(three,four,five,six)
+                num = "%s%s%s%s" % (three, four, five, six)
                 pds.append(num)
 
-
-
-file_object = open('/Users/edge/Desktop/pwdNum4.txt', 'w')
-file_object.writelines(['%s%s' % (x,os.linesep) for x in pds])
-file_object.close( )
+# file_object = open('/Users/edge/Desktop/pwdNum4.txt', 'w')
+file_object = open('C:\Users\edge\Desktop\pwdNum4.txt', 'w')
+file_object.writelines(['%s%s' % (x, os.linesep) for x in pds])
+file_object.close()
